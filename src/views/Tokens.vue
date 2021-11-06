b29d9b869a165f42f3b50f5c46696ba2ad36b7de
Cameron Howson <cameron@superrb.com>
2021-11-06 20:38:47 +0000

    tidying tokens page

--- src/views/Tokens.vue
+++ src/views/Tokens.vue
@@ -1,69 +1,93 @@
 <template>
-  <div id="tokens" class="relative max-w-screen-xl mx-auto items-center flex flex-1 justify-center xl:px-0 px-3 text-gray-500">
+  <div id="tokens" class="max-w-screen-xl mx-auto flex flex-1 flex-col items-center justify-center oswap-layout xl:px-0 px-3 text-gray-500 pb-16">
     <transition name="horizontal" appear>
-      <div id="tokenList" class="flex z-30 w-full rounded-b-3xl overflow-x-hidden" style="margin-top: 98px">
-        <perfect-scrollbar class="flex flex-col flex-1 divide-y divide-black divide-opacity-10 w-full overflow-hidden">
-          <div class="flex text-gray-500 dark:text-gray-300" v-for="(network, index) in this.retrieveTokens(search, 1666600000)" :key="index">
-            <div class="flex w-full flex-wrap gap-1 justify-between">
-              <!-- network name -->
-              <div class="flex w-full px-2 py-4 rounded-b-2xl items-center space-x-2">
-                <img :src="network.icon" class="h-4" alt="" />
-                <p class="flex text-sm items-center dark:text-gray-300">{{ network.name }}</p>
-              </div>
+      <div id="tokenList" class="flex z-30 w-full rounded-b-3xl" style="margin-top: 98px">
+        <div class="flex flex-col flex-1 divide-y divide-black divide-opacity-10 w-full">
+          <div class="flex flex-wrap text-gray-500 dark:text-gray-300" v-for="(network, index) in this.retrieveTokens(search, 1666600000)" :key="index">
+            <!-- network name -->
+            <div class="flex w-full px-2 py-4 rounded-b-2xl items-center space-x-2">
+              <img :src="network.icon" class="h-4" alt="" />
+              <p class="flex text-sm items-center dark:text-gray-300">{{ network.name }}</p>
+            </div>
+            <div class="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 w-full">
               <!-- icons table -->
-              <div v-for="(token, idx) in network.tokens[1666600000]" :key="idx" class="w-1/3 p-2 bg-gradient-to-l flex flex-wrap from-slightGray dark:from-slightDark to-transparent text-gray-500 dark:text-gray-300 ss:w-80 xs:w-96 rounded-3xl border-l border-oswapGreen mb-4 flex flex-col space-y-2 h-32 rounded-xl">
-                <div class="w-9/12">
-                  <img :src="token.imgSrc" alt="" class="h-8 w-8 rounded-full flex items-center justify-center" />
-                  <p class="text-xs text-oswapBlue-light">
-                    {{ token.name }}
-                    <span class="text-xs dark:text-gray-300">
-                      {{ token.Symbol }}
-                    </span>
-                  </p>
-                  <p class="text-xs flex-none dark:text-oswapGreen-dark">One Address</p>
-                  <p class="text-xs flex gap-1 dark:text-gray-300">
-                    {{ shortenAddress(token.oneAddress) }}
-                    <tooltip-me>
-                      <svg class="h-4 w-4 dark:text-oswapGreen-dark" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" />
-                        <rect x="8" y="8" width="12" height="12" rx="2" />
-                        <path d="M16 8v-2a2 2 0 0 0 -2 -2h-8a2 2 0 0 0 -2 2v8a2 2 0 0 0 2 2h2" />
-                      </svg>
-                      <tooltip-me-content :options="tooltip" class="flex ss:w-64 xs:w-80 rounded-lg shadow-xl p-0.5">
-                        <div class="flex w-full space-x-2 items-start bg-gray-100 dark:bg-slightDark text-gray-500 dark:text-gray-300 rounded-md p-3">
-                          <svg class="h-4 w-4 dark:text-oswapGreen-dark" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <div v-for="(token, idx) in network.tokens[1666600000]" :key="idx" class="flex flex-col p-3 st5 st5-all group bg-gradient-to-l dark:from-slightDark from-slightGray to-transparent dark:hover:bg-slightDark hover:bg-slightGray border-l border-oswapGreen rounded-3xl">
+                <div class="w-full flex flex-wrap">
+                  <div class="w-full relative flex gap-5 h-12 items-center">
+                    <img :src="token.imgSrc" alt="" class="h-8 w-8 rounded-full flex items-center justify-center" />
+                    <p class="text-xs text-oswapBlue-light">
+                      {{ token.name }}
+                      <span class="text-xs dark:text-gray-300">
+                        {{ token.Symbol }}
+                      </span>
+                    </p>
+                  </div>
+                  <div class="flex w-8/12 flex-col h-full mt-1 relative" v-if="token.oneAddress">
+                    <div class="flex gap-5 mb-2 w-6/12">
+                      <p class="text-xs flex-none dark:text-oswapGreen-dark">One Address</p>
+                      <p class="text-xs flex gap-1 dark:text-gray-300">
+                        {{ shortenAddress(token.oneAddress) }}
+                        <tooltip-me>
+                          <svg @click="copyAddress(token.oneAddress)" class="h-4 w-4 dark:text-oswapGreen-dark hover:opacity-50" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" />
                             <rect x="8" y="8" width="12" height="12" rx="2" />
                             <path d="M16 8v-2a2 2 0 0 0 -2 -2h-8a2 2 0 0 0 -2 2v8a2 2 0 0 0 2 2h2" />
                           </svg>
-                          <div class="flex flex-1 flex-col space-y-2">
-                            <div class="flex items-center text-xs">
-                              <p>Copy One Address</p>
+                          <tooltip-me-content :options="tooltip" class="flex ss:w-64 xs:w-80 rounded-lg shadow-xl p-0.5">
+                            <div class="flex w-full space-x-2 items-start bg-gray-100 dark:bg-slightDark text-gray-500 dark:text-gray-300 rounded-md p-3">
+                              <svg class="h-6 w-6 dark:text-oswapGreen-dark" width="36" height="36" viewBox="0 0 36 36" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" />
+                                <rect x="8" y="8" width="12" height="12" rx="2" />
+                                <path d="M16 8v-2a2 2 0 0 0 -2 -2h-8a2 2 0 0 0 -2 2v8a2 2 0 0 0 2 2h2" />
+                              </svg>
+                              <div class="flex flex-1 flex-col space-y-2">
+                                <div class="flex items-center text-xs">
+                                  <p>Copy One Address</p>
+                                </div>
+                              </div>
                             </div>
-                          </div>
-                        </div>
-                      </tooltip-me-content>
-                    </tooltip-me>
-                  </p>
-                  <p class="text-xs flex-none dark:text-oswapGreen-dark">Bsc Address</p>
-                  <p class="text-xs flex gap-1 dark:text-gray-300">
-                    {{ shortenAddress(token.bscAddress) }}
-                    <svg class="h-4 w-4 dark:text-oswapGreen-dark" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                      <path stroke="none" d="M0 0h24v24H0z" />
-                      <rect x="8" y="8" width="12" height="12" rx="2" />
-                      <path d="M16 8v-2a2 2 0 0 0 -2 -2h-8a2 2 0 0 0 -2 2v8a2 2 0 0 0 2 2h2" />
-                    </svg>
-                  </p>
-                </div>
-                <div class="w-3/12">
-                  <button class="flex items-center justify-center ml-3 rounded-full border border-oswapGreen st5 text-oswapGreen hover:bg-oswapGreen hover:text-gray-200 dark:hover:text-slightDark">
-                    <p class="text-sm p-1 px-3">Add to wallet</p>
-                  </button>
+                          </tooltip-me-content>
+                        </tooltip-me>
+                      </p>
+                    </div>
+                    <div class="flex gap-5 mt-1 mb-2 w-6/12" v-if="token.bscAddress">
+                      <p class="text-xs flex-none dark:text-oswapGreen-dark">Bsc Address</p>
+                      <p class="text-xs flex gap-1 dark:text-gray-300">
+                        {{ shortenAddress(token.bscAddress) }}
+                        <tooltip-me>
+                          <svg @click="copyAddress(token.bscAddress)" class="h-4 w-4 dark:text-oswapGreen-dark hover:opacity-50" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" />
+                            <rect x="8" y="8" width="12" height="12" rx="2" />
+                            <path d="M16 8v-2a2 2 0 0 0 -2 -2h-8a2 2 0 0 0 -2 2v8a2 2 0 0 0 2 2h2" />
+                          </svg>
+                          <tooltip-me-content :options="tooltip" class="flex ss:w-64 xs:w-80 rounded-lg shadow-xl p-0.5">
+                            <div class="flex w-full space-x-2 items-start bg-gray-100 dark:bg-slightDark text-gray-500 dark:text-gray-300 rounded-md p-3">
+                              <svg class="h-6 w-6 dark:text-oswapGreen-dark" width="36" height="36" viewBox="0 0 36 36" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" />
+                                <rect x="8" y="8" width="12" height="12" rx="2" />
+                                <path d="M16 8v-2a2 2 0 0 0 -2 -2h-8a2 2 0 0 0 -2 2v8a2 2 0 0 0 2 2h2" />
+                              </svg>
+                              <div class="flex flex-1 flex-col space-y-2">
+                                <div class="flex items-center text-xs">
+                                  <p>Copy Bsc Address</p>
+                                </div>
+                              </div>
+                            </div>
+                          </tooltip-me-content>
+                        </tooltip-me>
+                      </p>
+                    </div>
+                  </div>
+                  <div class="w-4/12">
+                    <button class="flex items-center justify-center ml-3 rounded-full border border-oswapGreen st5 text-oswapGreen hover:bg-oswapGreen hover:text-gray-200 dark:hover:text-slightDark">
+                      <p class="text-sm p-1 px-3">Add to wallet</p>
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </perfect-scrollbar>
+        </div>
       </div>
     </transition>
   </div>
@@ -71,6 +95,7 @@
 
 <script>
 import { mapGetters, mapActions } from 'vuex'
+import { toastMe } from '@/components/toaster/toaster.js'
 
 export default {
   name: 'Tokens',
@@ -82,7 +107,6 @@ export default {
   created() {
     window.addEventListener('keyup', this.doCommand)
     const tokens = this.retrieveTokens(this.search, 1666600000)
-    console.log(tokens)
   },
   unmounted() {
     window.removeEventListener('keyup', this.doCommand)
@@ -98,22 +122,26 @@ export default {
     ...mapActions('bridge', ['setToken', 'goTo']),
     ...mapGetters('bridge', ['getToken']),
 
-    doCommand(e) {
-      if (e.code == 'Escape') {
-        this.goTo('swap')
-      }
-    },
-
-    selectToken(token) {
-      this.setToken({ tokenRef: this.whichToken, token: token })
-      // Resets search field
-      this.search = ''
-      // closes the modal
-      this.goTo('swap')
-    },
-
     shortenAddress(address) {
       return address ? (address = `${address.slice(0, 3)}...${address.slice(address.length - 3, address.length)}`) : address
+    },
+
+    async copyAddress(address) {
+      console.log(`copying ${address}`)
+      try {
+        await navigator.clipboard.writeText(address)
+        toastMe('success', {
+          title: 'Sucessfully Copied',
+          link: false,
+          href: '',
+        })
+      } catch ($e) {
+        toastMe('warning', {
+          title: 'Error Copying',
+          link: false,
+          href: '',
+        })
+      }
     },
   },
 }
